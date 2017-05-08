d4f222d2db094e404ad4b597b7a7141d74d3dda5
pcorletto <peter.corletto@gmail.com>
2017-05-08 18:38:32 -0400

    Added a classroom sample class

---
+++ class_examples/classroom/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace classroom
+{
+    class Room
+    {
+
+        public int RoomNumber{get; set;}
+
+        public int Period{get; set;}
+
+        public string[] DaysOfWeek{get; set;}
+
+        public string TeacherName{get; set;}
+
+    }
+
+    class Student
+    {
+
+        public string Name{get; set;}
+
+        public int StudentID{get; set;}
+
+        public int[] roomNumbers{get; set;}
+
+        public float GPA{get; set;}
+
+    }
+
+    class Teacher
+    {
+        public string Name{get; set;}
+
+        public string Subject{get; set;}
+
+        public int Period{get; set;}
+
+        public int RoomID{get; set;}
+
+        public string[] DaysOfWeek{get; set;}
+
+    }
+
+    class Seat
+    {
+
+        public int SeatID{get; set;}
+
+        public int RoomNumber{get; set;}
+
+        // In a classroom 2-dimensional grid, what is the position
+        public int[] position{get; set;}
+
+        public id StudentID{get; set;}
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room firstRoom = new Room();
+            firstRoom.setTeacherName("Cecil Phillip");
+            firstRoom.setRoomNumber(8310);
+            firstRoom.setDaysOfWeek("Monday", "Wednesday");
+            firstRoom.setPeriod("6PM-9PM");
+        }
+    }
+}
